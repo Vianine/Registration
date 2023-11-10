580fca1c6505bca6d4e7ba1b0156263497036b64
vianine <djimafovianine@yahoo.fr>
2023-11-10 23:02:11 +0100

    nouveau changement

--- src/main/java/com/vianine/registration/RegistrationApplication.java
+++ src/main/java/com/vianine/registration/RegistrationApplication.java
@@ -17,28 +17,78 @@ public class RegistrationApplication {
 
 		ArrayList<User> etudiant = new ArrayList<User>();
 
-		System.out.println(pers2.toString());
-// element ajouter dans l'arrayList
-		etudiant.add(pers1);
-		etudiant.add(pers);
-		etudiant.add(pers2);
-		System.out.println(etudiant);
+	//	System.out.println(pers2.toString());
+
+
 		System.out.println("-----------------------------------------------------");
 
-		for (int i=0;i<etudiant.size();i++){
-			// parcourir
-			if(etudiant.get(i).getEmail()!="user@yahoo.fr"){
-				System.out.println(etudiant.get(i));
+
+			// ajout
+
+			if (pers1.getEmail() != "user@yahoo.fr") {
+				etudiant.add(pers1);
+				System.out.println(etudiant);
 			}
+
 			else {
 				System.out.println("n'est pas conforme");
 			}
 
-
-
+			 if (pers.getEmail() != "user@yahoo.fr") {
+			etudiant.add(pers);
+			System.out.println(etudiant);
 
 		}
-	}
+			 else {
+				 System.out.println("n'est pas conforme");
+			 }
 
+			  if (pers2.getEmail() != "user@yahoo.fr") {
+			etudiant.add(pers2);
+			System.out.println(etudiant);
+		}
+			  else {
+				  System.out.println("n'est pas conforme");
+			  }
+		}
 
-}
+			}
+
+//public class RegistrationApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(RegistrationApplication.class, args);
+//		User pers = new User( "Vianine","user@yahoo.com","1234");
+//		User pers1 = new User( "Djimafo","user@yahoo.fr","12345");
+//		User pers2 = new User( "Brile","user@yahoo.de","123456");
+//
+//		ArrayList<User> etudiant = new ArrayList<User>();
+//
+//		System.out.println(pers2.toString());
+//// element ajouter dans l'arrayList
+//		etudiant.add(pers1);
+//		etudiant.add(pers);
+//		etudiant.add(pers2);
+//		System.out.println(etudiant);
+//		System.out.println("-----------------------------------------------------");
+//
+//		for (int i=0;i<etudiant.size();i++){
+//			// parcourir
+//			if(etudiant.get(i).getEmail()!="user@yahoo.fr"){
+//				System.out.println(etudiant.get(i));
+//			}
+//			else {
+//				System.out.println("n'est pas conforme");
+//			}
+//
+//
+//
+//
+//		}
+//	}
+//
+//
+//}
+//
+//
+
